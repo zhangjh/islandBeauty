1bddc5b2b419af64da6ea5004bbae112bf83ff87
休竹 <jihong.zjh@alibaba-inc.com>
2016-08-24 14:14:22 +0800

    rm test files

--- bin/test.js
+++
@@ -1,8 +0,0 @@
-var request = require('request');
-var cheerio = require('cheerio');
-
-var query = encodeURIComponent("美人鱼");
-request("http://search.hdwan.net/cse/search?s=11504240492176070054&q=" + query,function(e,r,b){
-	var $ = cheerio.load(b);
-	console.log(b);
-});
