71c82416a876e586f15cbf274bc45a5212d1578d
休竹 <jihong.zjh@alibaba-inc.com>
2016-08-24 14:01:10 +0800

    add project files

---
+++ bin/main.js
@@ -0,0 +1,77 @@
+/*
+ * Des: islandBeauty - Node.js种子爬虫项目
+ * Author: njhxzhangjihong@126.com
+ * Date: 8/24 2016
+ * */
+
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var config = require('../conf/config');
+
+
+//加载网页解析库
+var Parser1 = require("../lib/parser1");
+var Parser2 = require("../lib/parser2"); 
+
+var args = process.argv.slice(2);
+
+var pattern = new RegExp("[\|\&\\s#\-\.\*\(\)\!\/]","g");
+
+if(!args.length){
+	//下载AV种子
+	var url = config["downloadSrc"][0];
+	var dst = config["downloadDir"][0];
+	var parser1 = new Parser1(url);
+	
+	parser1.getTorrentPageLinks().then(function(links){
+		for(var i=0,len=links.length;i<len;i++){
+			(function(i){
+				//console.log(links[i].href);
+				parser1.getTorrentDownloadLinks(links[i].href).then(function(downloadLink){
+					var name = links[i].title.replace(pattern,'');
+					parser1.download(downloadLink,dst,name);
+				});
+			})(i);
+		}
+	}).catch(function(e){
+		if(e)console.log(e);
+	});
+}else {
+	//下载电影种子
+	if(args[0] !== "search"){
+		console.log("Usage: node main.js search 'movie name'");
+		return false;
+	}
+
+	var movieUrlPre = config["downloadSrc"][1];
+	var url = movieUrlPre + "/cse/search?s=11504240492176070054&q=" + encodeURIComponent(args[1]);
+	var dst = config["downloadDir"][1];
+	var parser2 = new Parser2(url);
+
+	parser2.getTorrentPageLinks().then(function(link){
+		console.log(link);
+		if(link){
+			parser2.getTorrentDownloadLinks(link).then(function(downloadLinks){
+				if(downloadLinks.length){
+					for(var i=0,len=downloadLinks.length;i<len;i++){
+						(function(i){
+							var downloadLink = downloadLinks[i].href;
+							var name = downloadLinks[i].title.replace(pattern,'');
+							parser2.download(downloadLink,dst,name);
+						})(i);
+						
+					}
+				}else {
+					console.error("没有下载链接！");
+				}
+				
+			});
+		}else {
+			console.error("没有搜索结果！");
+		}
+		
+	}).catch(function(e){
+		console.log(e);
+	});
+}
---
+++ conf/config.js
@@ -0,0 +1,6 @@
+var conf = {
+	downloadSrc: ["http://www.torrentbit.net/cat/Adult/?list=all","http://search.hdwan.net"],
+    downloadDir: ["../dst/av","../dst/movie"]
+};
+
+module.exports = conf;
